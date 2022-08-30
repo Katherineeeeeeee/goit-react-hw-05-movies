8396b335adaac3fe2a12dc2464ff33c08c25a5be
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-08-30 17:45:57 +0300

    added nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+// import s from './App.module.css';
+
+import Home from '../views/Home';
+import Movies from '../views/Movies';
+import MovieDetails from '../views/MovieDetails';
+import NotFound from '../views/NotFound';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/movies/:movieId">Movie Details</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
-};
+}
