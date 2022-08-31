515b449268858ca6443093ec26b00dadd9c1ca00
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-08-31 14:46:08 +0300

    added api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
-// import s from './App.module.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from 'modules/Navigation/Navigation';
 
 import Home from '../views/Home';
 import Movies from '../views/Movies';
-import MovieDetails from '../views/MovieDetails';
 import NotFound from '../views/NotFound';
 
 export default function App() {
   return (
     <>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
-        <NavLink to="/movies/:movieId">Movie Details</NavLink>
-      </nav>
+      <header>
+        <Navigation />
+      </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
---
+++ src/modules/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import s from './Navigation.module.css';
+
+const isActive = ({ isActive }) => {
+  return isActive ? s.active : '';
+};
+
+export default function Navigation() {
+  return (
+    <>
+      <nav>
+        <NavLink to="/" className={isActive}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={isActive}>
+          Movies
+        </NavLink>
+        <NavLink to="/movies/:movieId" className={isActive}>
+          Movie Details
+        </NavLink>
+      </nav>
+    </>
+  );
+}
